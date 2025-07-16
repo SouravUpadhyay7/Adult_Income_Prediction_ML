62854192a02f19be2dae2783c4af225b845d1d62
sourav upadhyay <pkrsourav@gmail.com>
2025-07-16 19:47:59 +0530

    Day 6 -- Model Training done

--- src/component/data_ingestion.py
+++ src/component/data_ingestion.py
@@ -6,7 +6,7 @@ from src.exception import CustomException
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
 from src.component.data_transformation import DataTransformation
-#from src.component.modrl_trainer import ModelTrainer
+from src.component.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -54,8 +54,8 @@ if __name__ =="__main__":
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.inititate_data_transformation(train_data_path , test_data_path)
 
-    #modeltrainer = ModelTrainer()
-    #print(modeltrainer.inititate_model_trainer(train_arr, test_arr))
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.inititate_model_trainer(train_arr, test_arr))
 
 
 
